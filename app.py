05c62aa732114f6d5003aecd3cf5d0f4ddb38e3e
KhaledAlasmari <kalasmari@lucidya.com>
2023-11-18 00:07:42 +0300

    validate generate_story body

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-# save this as app.py
-from flask import Flask, request, jsonify, Response
-
+from flask import Flask, jsonify, g
+from flask_expects_json import expects_json
+from utils.schema.story_schema import generate_story_schema
 from ai.image_to_text import ImageToText
 
 app = Flask(__name__)
 
 @app.route('/generate_story', methods=['POST'])
+@expects_json(generate_story_schema)
 def generate_story():
-    pass
+    print(g.data)
+    return "", 200
 
 
 @app.route("/health")
