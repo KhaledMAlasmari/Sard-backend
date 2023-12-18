0655fd3d607aad77aefae3ae3e7516e292303092
KhaledAlasmari <kalasmari@lucidya.com>
2023-12-19 02:13:08 +0300

    Add routes for genres and authors

---
+++ models/authors.py
@@ -0,0 +1,10 @@
+class Authors:
+    @staticmethod
+    def get_all_authors():
+        return [
+            "J.K. Rowling",
+            "Agatha Christie",
+            "Stephen King",
+            "J.R.R. Tolkien",
+            "George R.R. Martin",
+        ].sort()
---
+++ models/genres.py
@@ -0,0 +1,13 @@
+class Genres:
+    @staticmethod
+    def get_all_genres():
+        return [
+            "Mystery",
+            "Science Fiction",
+            "Fantasy",
+            "Romance",
+            "Adventure",
+            "Historical",
+            "Horror",
+            "Action",
+        ].sort()
