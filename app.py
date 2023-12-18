f4a0e6b1da4e571cf1a96db773b0047c6ac3dd67
Omar Abdulbagi <ramoboy56@gmail.com>
2023-12-19 02:05:07 +0300

    intial socket server commit

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 from flask import Flask, g
+from flask_socketio import SocketIO, send, emit,join_room, leave_room
 from flask_expects_json import expects_json
 from utils.schema.story_schema import generate_story_schema
 from models.genres import Genres
 from models.authors import Authors
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
+@app.route('/')
+def index():
+    return "SocketIO Story Generator"
 
-@app.route("/generate_story", methods=["POST"])
-@expects_json(generate_story_schema)
-def generate_story():
-    print(g.data)
-    return "", 200
+@socketio.on('generate_story')
+# @expects_json(generate_story_schema)
+def handle_generate_story(data):
+    room = data['room']
+    story = "my story!"
+    emit("story",story, to=room)
 
+@socketio.on('connect')
+def test_connect(auth):
+    emit('my response', {'data': 'Connected'})
 
 @app.route("/genres", methods=["GET"])
 def get_genres():
@@ -27,3 +36,6 @@ def get_authors():
 @app.route("/health")
 def hello():
     return "", 201
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
---
+++ testSocket.py
@@ -0,0 +1,23 @@
+import socket
+import json
+import socketio
+
+
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print("Connected to server")
+
+@sio.event
+def disconnect():
+    print("Disconnected from server")
+
+if __name__ == "__main__":
+    server_url = "ws://127.0.0.1:5000"
+    sio.connect(server_url)
+    
+    message_to_send = {"guy":"khaled"}
+    sio.emit("generate_story", message_to_send)
+    
+    sio.wait()
